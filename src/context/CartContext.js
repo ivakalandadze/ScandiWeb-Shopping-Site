5d083bb3e1074eae150eccf98c597fc2f206e377
Iva Kalandadze <ivakalandadze@Ivas-MacBook-Air.local>
2022-09-14 20:46:50 +0400

    add item to cart update

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -12,57 +12,62 @@ export const CartConsumer = CartContext.Consumer
 
 export default class CartProvider extends Component {
     state = {
-        cartItems: [{"huarache-x-stussy-le": 1}]
+        cartItems: []
     }
 
-    getProducts = () =>{
+    getProducts = (id) =>{
       client
             .query({
                 query: gql`
-                query Query($input: CategoryInput) {
-                    category(input: $input) {
-                    products {
-                        inStock
-                        id
-                        name
-                        gallery
-                        description
-                        prices {
-                        currency {
-                            label
-                            symbol
-                        }
-                        amount
-                        }
-                        brand
-                    }
-                    }
+                query Query($productId: String!) {
+                  product(id: $productId) {
+                    id
+                    name
+                  }
                 }
                 `,
                 variables: {
-                    "input": {
-                        "title": this.props.category
-                    }
+                   "productId": id
                 }
             })
-            .then(result => this.setState({products:result.data.category.products})); 
+            .then(result=>{
+              const product = result.data.product
+              this.setState(prevState=>({
+              cartItems:[...prevState.cartItems, {[id]:{
+                "count": 1,
+                product
+              }}]
+            }))})
     }
 
     addItemtoCart = (item) => {
-      this.setState(prevState=>{
-        const existingItem = prevState.cartItems.find(prevItem=>Object.keys(prevItem)[0]===item.id)
-        if(existingItem){
-          const prevItems = [...prevState.cartItems]
-          console.log("shevida")
-          const index = prevState.cartItems.indexOf(existingItem)
-          console.log(prevItems[index][item.id])
-          prevItems[index][item.id] ++
-          return {cartItems: prevItems}
-        }
-        else{
-          return {cartItems:[...prevState.cartItems,{[item.id]:1}]}
-        }
-      })
+      // this.setState(prevState=>{
+      //   const existingItem = prevState.cartItems.find(prevItem=>Object.keys(prevItem)[0]===item.id)
+      //   if(existingItem){
+      //     const prevItems = [...prevState.cartItems]
+      //     console.log("shevida")
+      //     const index = prevState.cartItems.indexOf(existingItem)
+      //     console.log(prevItems[index][item.id])
+      //     prevItems[index][item.id]["count"] ++
+      //     return {cartItems: prevItems}
+      //   }
+      //   else{
+      //     // return {cartItems:[...prevState.cartItems,{[item.id]:
+      //     //   {"count" : 1}}]}
+      //     this.getProducts(item.id)
+      //   }
+      // })
+      const existingItem = this.state.cartItems.find(prevItem=>Object.keys(prevItem)[0]===item.id)
+      if(existingItem){
+        const prevItems = [...this.state.cartItems]
+        console.log("shevida")
+        const index = this.state.cartItems.indexOf(existingItem)
+        console.log(prevItems[index][item.id])
+        prevItems[index][item.id]["count"] ++
+        this.setState({cartItems: prevItems})
+      }else {
+        this.getProducts(item.id)
+      }
     }
   render() {
     const {cartItems} = this.state;
